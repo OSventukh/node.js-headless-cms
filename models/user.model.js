ed275d77f48dc0781eff8f1fefaf1ddfbc2799dd
Oleksandr Sventukh <o.sventukh@gmail.com>
2023-02-07 14:05:31 +0200

    Create user model

---
+++ models/user.model.js
@@ -0,0 +1,43 @@
+import { Model } from 'sequelize';
+
+export default (sequelize, DataTypes) => {
+  class User extends Model {
+    static associate(models) {
+      // define association here
+    }
+  }
+
+  User.init(
+    {
+      firstname: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      lastname: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+        unique: true,
+      },
+      password: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      role: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      topics: {
+        type: DataTypes.TEXT,
+      },
+    },
+    {
+      sequelize,
+      modelName: 'User',
+    },
+  );
+  return User;
+};
