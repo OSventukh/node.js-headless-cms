7e9bb1eb8fa518dbc9c54f4d9d5a13927803fd2d
Oleksandr Sventukh <o.sventukh@gmail.com>
2023-02-09 13:21:32 +0200

    Add topic database  model

---
+++ models/topic.model.js
@@ -0,0 +1,42 @@
+import { Model } from 'sequelize';
+
+export default (sequelize, DataTypes) => {
+  class Post extends Model {
+    static associate(models) {
+      // define association here
+    }
+  }
+
+  Post.init({
+    title: {
+      type: DataTypes.STING,
+      allowNull: false,
+    },
+    slug: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+    status: {
+      type: DataTypes.STRING,
+      defaultValue: 'active',
+      validate: {
+        isIn: {
+          args: [['active', 'inactive']],
+          msg: 'Incorect topic status value',
+        },
+      },
+    },
+  }, {
+    sequelize,
+    modelName: 'Topic',
+  });
+  return Post;
+};
