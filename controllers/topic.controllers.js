4b9745e4a38bd88a801a9fc5528ede610587b9de
Oleksandr Sventukh <o.sventukh@gmail.com>
2023-02-09 18:47:09 +0200

    Add service and controller for getting topics

--- controllers/topic.controllers.js
+++ controllers/topic.controllers.js
@@ -5,9 +5,53 @@ import {
   deleteTopicService,
 } from '../services/topic.services';
 
-export const createTopic = async (req, res, next) => {};
+export const createTopic = async (req, res, next) => {
+  const { title, slug, image, description, status } = req.body;
 
-export const getTopics = async (req, res, next) => {};
+  try {
+    await createTopicService(title, slug, image, description, status);
+    res.status(201).json({
+      message: 'Topic successfully created',
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: 'Could not create topic',
+    });
+  }
+};
+
+export const getTopics = async (req, res, next) => {
+  const { topicId } = req.params;
+  const { id, title, slug, description, status } = req.query;
+
+  let whereOptions;
+  if (topicId) {
+    whereOptions = {
+      id: topicId,
+    };
+  }
+
+  if (id || title || slug || description || status) {
+    whereOptions = {
+      ...(id && { id }),
+      ...(title && { title }),
+      ...(slug && { slug }),
+      ...(description && { description }),
+      ...(status && { status }),
+    };
+  }
+
+  try {
+    const topics = await getTopicsService(whereOptions);
+    res.status(200).json({
+      topics,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: 'Could not find topic(s)',
+    });
+  }
+};
 
 export const updateTopic = async (req, res, next) => {};
 
--- services/topic.services.js
+++ services/topic.services.js
@@ -1,8 +1,44 @@
 import db from '../models/index.js';
 
-export const createTopicService = async () => {};
+export const createTopicService = async (
+  title,
+  slug,
+  image,
+  description,
+  status,
+) => {
+  try {
+    const { Topic } = await db;
+    const topic = await Topic.create({
+      title,
+      slug,
+      image,
+      description,
+      status,
+    });
+    return topic;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
 
-export const getTopicsService = async () => {};
+export const getTopicsService = async (
+  whereOptions = {},
+  includeOptions = [],
+  orderOptions = [],
+) => {
+  try {
+    const { Topic } = await db;
+    const topics = await Topic.findAll({
+      where: whereOptions,
+      include: includeOptions,
+      order: orderOptions,
+    });
+    return topics;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
 
 export const updateTopicService = async () => {};
 
