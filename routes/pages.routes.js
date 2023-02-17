62d338bf910d756806216a241e7a77d865130302
Oleksandr Sventukh <o.sventukh@gmail.com>
2023-02-17 13:50:28 +0200

    Create routes and CRUD operations for pages

--- routes/pages.routes.js
+++ routes/pages.routes.js
@@ -1,6 +1,25 @@
 import express from 'express';
+import {
+  createPage,
+  getPages,
+  updatePage,
+  deletePage,
+} from '../controllers/page.controllers.js';
+
 const router = express.Router();
 
-router.post('/pages', () => {});
+router.get('/pages/:pageId', getPages);
+
+router.get('/pages', getPages);
+
+router.post('/pages', createPage);
+
+router.patch('/pages/:pageId', updatePage);
+
+router.patch('/pages', updatePage);
+
+router.delete('/pages/:pageId', deletePage);
+
+router.delete('/pages', deletePage);
 
 export default router;
