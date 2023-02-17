e32ced0abe6fa19ebc265ac436da1d1d792b4d66
Oleksandr Sventukh <o.sventukh@gmail.com>
2023-02-17 14:56:47 +0200

    Create router, controllers, model, and unit tests for users feature

---
+++ controllers/user.controllers.js
@@ -0,0 +1,116 @@
+import { Op } from 'sequelize';
+
+import {
+  createService,
+  getService,
+  updateService,
+  deleteService,
+} from '../services/services.js';
+
+import db from '../models/index.js';
+
+const { User } = db;
+
+export const createUser = async (req, res, next) => {
+  try {
+    await createService(User, req.body);
+    res.status(201).json({
+      message: 'User successfully created',
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: 'Could not create user',
+    });
+  }
+};
+
+export const getUsers = async (req, res, next) => {
+  // receive user id from url params or query
+  const id = req.params.userId || req.query.id;
+  // receive other parameters from url query
+  const { title, slug, description, status } = req.query;
+
+  // If parameter was provided add it to object which will be passed as where query to sequelize
+  const whereOptions = {
+    ...(id && { id }),
+    ...(title && { title }),
+    ...(slug && { slug }),
+    ...(description && { description }),
+    ...(status && { status }),
+  };
+
+  try {
+    // getting users with provided paramaters and response it to the client
+    const users = await getService(User, whereOptions);
+    res.status(200).json({
+      users,
+    });
+  } catch (error) {
+    res.status(404).json({
+      message: 'Could not find user(s)',
+    });
+  }
+};
+
+export const updateUser = async (req, res, next) => {
+  // Receive user id from url params or request body
+  const userId = req.params.userId || req.body.id;
+
+  // Divide the request body into data that will be updated and user id if it was passed.
+  // Id should remain unchanged
+  const { id, ...toUpdate } = req.body;
+
+  try {
+    // Check if user or users with provided id are exists
+    const user = await getService(User, { id: userId });
+
+    if (!user || user.length === 0) {
+      throw new Error('This user does not exist');
+    }
+
+    // update existing user
+    await updateService(User, toUpdate, { id: userId });
+
+    res.status(200).json({
+      message: 'User was successfully updated',
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: 'Could not update this user',
+    });
+  }
+};
+
+export const deleteUser = async (req, res, next) => {
+  // receive user id from url params or request body
+  let userId = req.params.userId || req.body.id;
+
+  // transform user id to array if it is not
+  if (userId && !Array.isArray(userId)) {
+    userId = [userId];
+  }
+
+  try {
+    // Checking if user or users with passed id(s) is exists
+    const users = await getService(User, {
+      id: { [Op.in]: userId },
+    });
+
+    if (!users || users.length === 0) {
+      throw new Error('This user does not exist');
+    }
+    // deleting all users with given id
+    Promise.all(
+      userId.map(async (id) => {
+        await deleteService(User, { id });
+      }),
+    );
+    res.status(200).json({
+      message: 'User was succesfully deleted',
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: 'Could not delete user',
+    });
+  }
+};
---
+++ models/user.model.js
@@ -0,0 +1,34 @@
+import { Model } from 'sequelize';
+
+export default (sequelize, DataTypes) => {
+  class User extends Model {
+    static associate(models) {
+      // define association here
+    }
+  }
+
+  User.init({
+    fistname: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: false,
+    },
+    lastname: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  }, {
+    sequelize,
+    modelName: 'User',
+  });
+  return User;
+};
