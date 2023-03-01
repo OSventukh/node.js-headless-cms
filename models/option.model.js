9f21a544352355236d7d743f34aa4fbcfdc8480c
Oleksandr Sventukh <o.sventukh@gmail.com>
2023-03-01 22:48:43 +0200

    Create Option model

---
+++ models/option.model.js
@@ -0,0 +1,39 @@
+import { Model } from 'sequelize';
+
+export default (sequelize, DataTypes) => {
+  class Option extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+
+  Option.init(
+    {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          notEmpty: true,
+        },
+      },
+      value: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: true,
+        },
+      },
+    },
+    {
+      sequelize,
+      modelName: 'Option',
+    },
+  );
+  return Option;
+};
