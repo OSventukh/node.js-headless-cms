88a0632f198475b5c0cfe38c93cbd743845fb144
Oleksandr Sventukh <o.sventukh@gmail.com>
2023-03-02 00:41:11 +0200

    Add controllers to options

--- controllers/options.controllers.js
+++ controllers/options.controllers.js
@@ -1,7 +1,73 @@
-export const createOptionController = async () => {};
+import HttpError from '../utils/http-error.js';
 
-export const getOptionsController = async () => {};
+import {
+  createOption,
+  getOptions,
+  updateOption,
+  deleteOption,
+} from '../services/options.services.js';
 
-export const updateOptionController = async () => {};
+export const createOptionController = async (req, res, next) => {
+  try {
+    const option = await createOption(req.body);
+    res.status(201).json({
+      message: 'Option successfully created',
+      option,
+    });
+  } catch (error) {
+    next(new HttpError(error.message, error.statusCode));
+  }
+};
 
-export const deleteOptionController = async () => {};
+export const getOptionsController = async (req, res, next) => {
+  // Receive option name from url params or query
+  const name = req.params.optionName;
+
+  try {
+    // get topics with provided parameters and response it to the client
+    const { count, rows } = await getOptions({
+      name,
+      ...req.query,
+    });
+    res.status(200).json({
+      count,
+      options: rows,
+    });
+  } catch (error) {
+    next(new HttpError(error.message, error.statusCode));
+  }
+};
+
+export const updateOptionController = async (req, res, next) => {
+  // Receive option name from url params or request body
+  const optionName = req.params.optionName || req.body.name;
+
+  // Divide the request body into data that will be updated and option id
+  // Option name should reamin unchanged
+  const { name, ...toUpdate } = req.body;
+
+  try {
+    await updateOption(optionName, toUpdate);
+
+    res.status(200).json({
+      message: 'Option was successfully updated',
+    });
+  } catch (error) {
+    next(new HttpError(error.message, error.statusCode));
+  }
+};
+
+export const deleteOptionController = async (req, res, next) => {
+  // Receive option name from url param or request body
+  const optionName = req.params.optionName || req.body.name;
+
+  try {
+    await deleteOption(optionName);
+
+    res.status(200).json({
+      message: 'Option was successfully deleted',
+    });
+  } catch (error) {
+    next(new HttpError(error.message, error.statusCode));
+  }
+};
