08cdeaa8ec5fbe7eccce3c1d5c3e0b57981fdf71
Oleksandr Sventukh <o.sventukh@gmail.com>
2023-03-07 16:20:50 +0200

    test: add unit test to auth middleware

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,21 +3,21 @@ import HttpError from '../utils/http-error.js';
 import { UserBlockedToken } from '../models/index.js';
 
 export default async (req, res, next) => {
-  if (req.path === '/login' || req.path === '/login/refreshtoken') {
-    return next();
-  }
   try {
     const authHeader = req.get('authorization');
-    const token = authHeader.split(' ')[1];
-
+    const token = authHeader?.split(' ')[1];
     // Check if the accessToken provided by the user is not already blocked.
+    if (!token) {
+      throw new HttpError('Not Authenticated', 401);
+    }
+
     const blockedToken = await UserBlockedToken.findOne({
       where: {
         token,
       },
     });
-
-    if (!token || blockedToken) {
+    console.log('token', authHeader)
+    if (blockedToken) {
       throw new HttpError('Not Authenticated', 401);
     }
     const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET_KEY);
