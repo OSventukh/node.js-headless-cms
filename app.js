c29411991599d09c3655bdae2dcfcd1e0d8ef544
Oleksandr Sventukh <o.sventukh@gmail.com>
2023-03-30 20:36:09 +0300

    Create cron job to delete expired tokens

--- app.js
+++ app.js
@@ -2,14 +2,16 @@ import express from 'express';
 import cookieParser from 'cookie-parser';
 import routes from './routes.js';
 import errorHandler from './middlewares/error-handle.js';
+import deleteExpiredTokenCron from './cron/deleteExpiredToken.js';
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
 
-await routes(app);
+deleteExpiredTokenCron.start();
 
+await routes(app);
 app.use(errorHandler);
 
 export default app;
---
+++ cron/deleteExpiredToken.js
@@ -0,0 +1,26 @@
+import { CronJob }  from 'cron';
+import { Op } from 'sequelize';
+
+import { UserToken, UserBlockedToken } from '../models/index.js';
+
+// This cron runs every day at midnight
+const deleteExpiredTokenCron = new CronJob('00 00 00 * * *', () => {
+  Promise.all([
+    UserToken.destroy({
+      where: {
+        expiresIn: {
+          [Op.lte]: new Date(),
+        },
+      },
+    }),
+    UserBlockedToken.destroy({
+      where: {
+        expiresIn: {
+          [Op.lte]: new Date(),
+        },
+      },
+    }),
+  ]);
+});
+
+export default deleteExpiredTokenCron;
