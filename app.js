52a6a648f27436ce9dd9d68d874329e1519716a7
Oleksandr Sventukh <o.sventukh@gmail.com>
2023-04-09 21:42:23 +0300

    Add cors middleware

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 import routes from './routes.js';
 import errorHandler from './middlewares/error-handle.js';
 
@@ -7,6 +8,9 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+  origin: process.env.ACCESS_DOMEN || true,
+}));
 
 await routes(app);
 
